d4471ea9291c52aa3781fa8195584b1d894d295f
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2022-05-18 17:10:43 +0300

    added seek bar

--- src/renderer/views/Home.vue
+++ src/renderer/views/Home.vue
@@ -4,6 +4,7 @@ import { useState } from "../../renderer/main";
 const state = useState();
 const sound = ref<HTMLAudioElement>();
 const volume = computed(() => sound.value?.volume);
+const currentTime = computed(() => sound.value?.currentTime);
 onMounted(() => {
 	sound.value = new Audio(state.openedFile);
 	sound.value.play();
@@ -15,6 +16,7 @@ onMounted(() => {
     Opened File: {{ state.openedFile }}
     <br>
     Volume: <input v-model="sound.volume" min="0" max="1" step="0.01" type="range"> {{ volume }}
+    Seek: <input v-model="currentTime" min="0" :max="sound.duration" step="0.01" type="range">
     <br>
     <button @click="sound!.play()">
       play
