2313e6ccb32ba01c0fb8d44060944d99b7ff43d1
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2022-05-18 17:20:07 +0300

    update logo

--- src/renderer/views/Home.vue
+++ src/renderer/views/Home.vue
@@ -4,7 +4,6 @@ import { useState } from "../../renderer/main";
 const state = useState();
 const sound = ref<HTMLAudioElement>();
 const volume = computed(() => sound.value?.volume);
-const currentTime = computed(() => sound.value?.currentTime);
 onMounted(() => {
 	sound.value = new Audio(state.openedFile);
 	sound.value.play();
@@ -16,7 +15,7 @@ onMounted(() => {
     Opened File: {{ state.openedFile }}
     <br>
     Volume: <input v-model="sound.volume" min="0" max="1" step="0.01" type="range"> {{ volume }}
-    Seek: <input v-model="currentTime" min="0" :max="sound.duration" step="0.01" type="range">
+    Seek: <input v-model="sound.currentTime" min="0" :max="sound.duration" step="0.01" type="range">
     <br>
     <button @click="sound!.play()">
       play
