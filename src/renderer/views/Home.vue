8f487f09defe15dbe981852a071c96b231bc87ca
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2022-05-18 17:32:03 +0300

    fix duration not updating after rerender

--- src/renderer/views/Home.vue
+++ src/renderer/views/Home.vue
@@ -4,6 +4,7 @@ import { useState } from "../../renderer/main";
 const state = useState();
 const sound = ref<HTMLAudioElement>();
 const volume = computed(() => sound.value?.volume);
+const duration = computed(() => sound.value?.duration);
 onMounted(() => {
 	sound.value = new Audio(state.openedFile);
 	sound.value.play();
@@ -15,7 +16,7 @@ onMounted(() => {
     Opened File: {{ state.openedFile }}
     <br>
     Volume: <input v-model="sound.volume" min="0" max="1" step="0.01" type="range"> {{ volume }}
-    Seek: <input v-model="sound.currentTime" min="0" :max="sound.duration" step="0.01" type="range">
+    Seek: <input v-model="sound.currentTime" min="0" :max="duration" step="0.01" type="range">
     <br>
     <button @click="sound!.play()">
       play
