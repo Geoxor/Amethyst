008322bb033eee61abe6dc4113ec164794f1d246
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2022-05-23 06:27:12 +0300

    fix #11

--- src/renderer/components/Explorer.vue
+++ src/renderer/components/Explorer.vue
@@ -6,9 +6,9 @@ const state = useState();
 const isHoldingControl = useKeyModifier("Control");
 const invoke = window.electron.ipcRenderer.invoke;
 
-const parseTitles = (path: string, trim: number) => {
+const parseTitle = (path: string, trim: number) => {
   // Get everything after the last \
-  const fileName = path.substring(path.lastIndexOf("\\") + 1);
+  const fileName = path.substring(Math.max(path.lastIndexOf("\\"), path.lastIndexOf("/")) + 1);
 
   // Trim the text and remove ending space
   const trimmed = fileName.substring(0, trim).trimEnd();
@@ -41,7 +41,7 @@ const parseTitles = (path: string, trim: number) => {
         <cover class="inline align-top w-3 h-3" :song-path="song" />
 
         <p class=" inline align-top text-xs ml-2 max-w-40 overflow-hidden overflow-ellipsis ">
-          {{ i === state.state.player.getCurrentlyPlayingIndex() ? "⏵ " : "" }}{{ parseTitles(song, i === state.state.player.getCurrentlyPlayingIndex() ? 30 : 32) }}
+          {{ i === state.state.player.getCurrentlyPlayingIndex() ? "⏵ " : "" }}{{ parseTitle(song, i === state.state.player.getCurrentlyPlayingIndex() ? 30 : 32) }}
         </p>
       </li>
     </ul>
