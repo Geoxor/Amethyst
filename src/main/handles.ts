fffac0ae7f3ea7ff128d35b60e8ceecc343c30e7
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2022-06-08 13:54:57 +0300

    ok revert that

--- src/main/handles.ts
+++ src/main/handles.ts
@@ -4,20 +4,24 @@ import { ALLOWED_EXTENSIONS } from "./main";
 
 export type FileTree = (string | string[])[];
 
-export async function loadFolder(inputPath: string): Promise<FileTree> {
-	const firstFolder = await fs.promises.readdir(inputPath);
-
-	const files = await Promise.all(
-		firstFolder.map(async (file) => {
-			const filePath = path.join(inputPath, file);
-			const stats = await fs.promises.stat(filePath);
-			if (stats.isDirectory())
-				return loadFolder(filePath);
-			else if (stats.isFile() && ALLOWED_EXTENSIONS.includes(path.extname(filePath).slice(1)))
-				return filePath;
-			else return undefined;
-		}),
-	);
-
-	return files.filter(file => !!file) as FileTree;
+export async function loadFolder(inputPath: string) {
+	return new Promise((resolve, reject) => {
+		fs.readdir(inputPath, (error, files) => {
+			if (error) {
+				reject(error);
+			}
+			else {
+				Promise.all(
+					files.map(async (file) => {
+						const filePath = path.join(inputPath, file);
+						const stats = await fs.promises.stat(filePath);
+						if (stats.isDirectory())
+							return loadFolder(filePath);
+						else if (stats.isFile() && ALLOWED_EXTENSIONS.includes(path.extname(filePath).slice(1)))
+							return filePath;
+					}),
+				).then(files => resolve(files.filter(file => !!file)));
+			}
+		});
+	});
 }
