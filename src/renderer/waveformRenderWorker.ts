dac39d630b7c16ebf52f749aa8c05ff18ec37210
Yukiiro-Nite <e.kazuma@gmail.com>
2022-06-21 21:37:59 -0400

    Issue #7 | Added audio waveform under progress bar

---
+++ src/renderer/waveformRenderWorker.ts
@@ -0,0 +1,51 @@
+let stopRender = false;
+
+function render(canvas: HTMLCanvasElement, audioData: Float32Array): ImageBitmap | undefined {
+  const ctx = canvas.getContext('2d');
+
+  if (!ctx) return;
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  const gradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
+  gradient.addColorStop(0, '#e08eff')
+  gradient.addColorStop(0.5, '#868aff')
+  gradient.addColorStop(1, '#e08eff')
+
+  ctx.strokeStyle = gradient;
+  ctx.lineWidth = 1;
+  ctx.beginPath()
+  ctx.moveTo(0, canvas.height / 2)
+
+  let x = 0;
+  let y = 0;
+  for (let i = 0; (i < audioData.length || stopRender); i++) {
+    y = (audioData[i] * (canvas.height / 2)) + (canvas.height / 2);
+    x = (i / audioData.length) * canvas.width;
+    ctx.lineTo(x, y);
+  }
+
+  if (stopRender) return;
+
+  ctx.stroke();
+
+  // Need to create an image from the OffscreenCanvas to send it back to the main process.
+  // https://developer.mozilla.org/en-US/docs/Web/API/OffScreenCanvas/transferToImageBitmap
+  // @ts-ignore
+  const img: ImageBitmap = canvas.transferToImageBitmap()
+  return img
+}
+
+onmessage = function(e: MessageEvent<{ canvas: HTMLCanvasElement, audioData: Float32Array, stop: boolean }>) {
+  const canvas = e.data.canvas;
+  const audioData = e.data.audioData;
+  stopRender = e.data.stop;
+
+  if (stopRender) return;
+
+  const img = render(canvas, audioData)
+
+  if (img) {
+    this.postMessage(img)
+  }
+};
