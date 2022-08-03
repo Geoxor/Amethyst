48c4c1b17abb2be3b911cc3d465a7c97bc2af5c3
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2022-08-03 22:59:05 +0300

    flipped notification icon

--- src/renderer/icons/BellIcon.vue
+++ src/renderer/icons/BellIcon.vue
@@ -1,8 +1,8 @@
 <template>
-  <svg width="16" height="16" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg width="14" height="16" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path fill="currentColor"
-      d="M7 0H6v1h1zM8 0H7v1h1zM9 0H8v1h1zM3 2h1v1H3zM4 1h1v1H4zM5 1h1v1H5zM10 1h1v1h-1zM11 1h1v1h-1zM12 2h1v1h-1zM2 8H1v4h1zM4 12H2v1h2zM3 3h1v1H3zM4 14h1v1H4zM4 13h1v1H4z" />
+      d="M8 0h1v1H8zM7 0h1v1H7zM6 0h1v1H6zM12 2h-1v1h1zM11 1h-1v1h1zM10 1H9v1h1zM5 1H4v1h1zM4 1H3v1h1zM3 2H2v1h1zM13 8h1v4h-1zM11 12h2v1h-2zM12 3h-1v1h1zM11 14h-1v1h1zM11 13h-1v1h1z" />
     <path fill="currentColor"
-      d="M4 13h1v1H4zM8 13h1v1H8zM2 4h1v4H2zM9 0h1v1H9zM14 12h1v1h-1zM1 12h1v1H1zM14 8h1v4h-1zM9 12h5v1H9zM5 15h3v1H5zM13 3h-1v1h1zM9 14H8v1h1zM14 4h-1v4h1z" />
+      d="M11 13h-1v1h1zM7 13H6v1h1zM13 4h-1v4h1zM6 0H5v1h1zM1 12H0v1h1zM14 12h-1v1h1zM1 8H0v4h1zM6 12H1v1h5zM10 15H7v1h3zM2 3h1v1H2zM6 14h1v1H6zM1 4h1v4H1z" />
   </svg>
 </template>
