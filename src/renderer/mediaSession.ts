3325815a6974a86fa16421c974902e384bc8d07a
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2022-09-24 02:24:29 +0300

    Update mediaSession.ts

--- src/renderer/mediaSession.ts
+++ src/renderer/mediaSession.ts
@@ -2,19 +2,19 @@ import Player from "./player";
 export default class MediaSession {
   public constructor(public player: Player) {
     const actionHandlers: ([MediaSessionAction, MediaSessionActionHandler])[] = [
-      ['play',          () => this.player.isPlaying() ? this.player.pause() : this.player.play()],
-      ['pause',         () => !this.player.isPlaying() ? this.player.play() : this.player.pause()],
+      ['play', () => this.player.isPlaying() ? this.player.pause() : this.player.play()],
+      ['pause', () => !this.player.isPlaying() ? this.player.play() : this.player.pause()],
       ['previoustrack', () => this.player.previous()],
-      ['nexttrack',     () => this.player.next()],
-      ['seekbackward',  (details) => { this.player.seekBackward(details.seekOffset || undefined) }],
-      ['seekforward',   (details) => { this.player.seekForward(details.seekOffset || undefined) }],
-      ['seekto',        (details) => { details.seekTime && this.player.seekTo(details.seekTime) }],
-      ['stop',          () => { 
+      ['nexttrack', () => this.player.next()],
+      ['seekbackward', (details) => { this.player.seekBackward(details.seekOffset || undefined) }],
+      ['seekforward', (details) => { this.player.seekForward(details.seekOffset || undefined) }],
+      ['seekto', (details) => { details.seekTime && this.player.seekTo(details.seekTime) }],
+      ['stop', () => {
         this.player.pause();
         this.player.setCurrentlyPlayingIndex(0);
       }],
     ];
-    
+
     for (const [action, handler] of actionHandlers) {
       try {
         navigator.mediaSession.setActionHandler(action, handler);
