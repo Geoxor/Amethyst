4813b9585d01de6c6798d5ca9eae3bbcb17d51ae
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2022-09-23 19:59:35 +0300

    small refactore

--- src/renderer/mediaSession.ts
+++ src/renderer/mediaSession.ts
@@ -2,25 +2,17 @@ import Player from "./player";
 export default class MediaSession {
   public constructor(public player: Player) {
     const actionHandlers: ([MediaSessionAction, MediaSessionActionHandler])[] = [
-      ['play',          () => { 
-        this.player.isPlaying() ? this.player.pause() : this.player.play();
-      }],
-      ['pause',         () => { 
-        !this.player.isPlaying() ? this.player.play() : this.player.pause();
-      }],
-      ['previoustrack', () => {  
-        this.player.previous();
-      }],
-      ['nexttrack',     () => { 
-        this.player.next();
-      }],
+      ['play',          () => this.player.isPlaying() ? this.player.pause() : this.player.play()],
+      ['pause',         () => !this.player.isPlaying() ? this.player.play() : this.player.pause()],
+      ['previoustrack', () => this.player.previous()],
+      ['nexttrack',     () => this.player.next()],
+      ['seekbackward',  (details) => { this.player.seekBackward(details.seekOffset || undefined) }],
+      ['seekforward',   (details) => { this.player.seekForward(details.seekOffset || undefined) }],
+      ['seekto',        (details) => { details.seekTime && this.player.seekTo(details.seekTime) }],
       ['stop',          () => { 
         this.player.pause();
         this.player.setCurrentlyPlayingIndex(0);
       }],
-      ['seekbackward',  (details) => { this.player.seekBackward(details.seekOffset || undefined) }],
-      ['seekforward',   (details) => { this.player.seekForward(details.seekOffset || undefined) }],
-      ['seekto',        (details) => { details.seekTime && this.player.seekTo(details.seekTime) }],
     ];
     
     for (const [action, handler] of actionHandlers) {
@@ -31,25 +23,14 @@ export default class MediaSession {
       }
     }
 
-    this.player.on("play", () => {
-      navigator.mediaSession.playbackState = "playing";
-    })
-
-    this.player.on("pause", () => {
-      navigator.mediaSession.playbackState = "paused";
-    })
+    this.player.on("play", () => navigator.mediaSession.playbackState = "playing")
+    this.player.on("pause", () => navigator.mediaSession.playbackState = "paused")
 
     this.player.on("metadata", meta => {
       const cover = meta?.common?.picture?.[0];
       let coverUrl: string = "";
 
-      if (cover) {
-        const coverBlob = new Blob([
-          new Uint8Array(cover.data, 0, cover.data.byteLength)
-        ], { type: "image/png" });
-        coverUrl = URL.createObjectURL(coverBlob);
-      }
-
+      cover && (coverUrl = URL.createObjectURL(new Blob([new Uint8Array(cover.data)], { type: "image/png" })));
 
       navigator.mediaSession.metadata = new MediaMetadata({
         title: meta.common.title,
