981d8f2e445787a77a8d52d79fa51a7cf72cf7e4
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2022-11-13 19:29:25 +0200

    Update PlaygroundView.vue

--- src/renderer/views/PlaygroundView.vue
+++ src/renderer/views/PlaygroundView.vue
@@ -8,7 +8,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="text-12px p-2">
+  <div class="text-12px p-2 w-full borderRight">
     <header class="italic opacity-50 text-primary-1000">
       This is the playground area, use this view to test out and develop components
     </header>
