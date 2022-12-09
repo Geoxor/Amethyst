b93373eeaef2664b3d3ed52ccb2959bfb93c8586
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2022-12-09 13:57:03 +0200

    update more icons

---
+++ src/renderer/icons/plumpy/PauseIcon.vue
@@ -0,0 +1,57 @@
+<template>
+  <svg
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    viewBox="0 0 172 172"
+    width="16"
+    height="16"
+  >
+    <g
+      fill="none"
+      fill-rule="nonzero"
+      stroke="none"
+      stroke-width="1"
+      stroke-linecap="butt"
+      stroke-linejoin="miter"
+      stroke-miterlimit="10"
+      stroke-dasharray=""
+      stroke-dashoffset="0"
+      font-family="none"
+      font-weight="none"
+      font-size="none"
+      text-anchor="none"
+      style="mix-blend-mode: normal"
+    >
+      <path
+        d="M0,172v-172h172v172z"
+        fill="none"
+      />
+      <g fill="currentColor">
+        <path
+          d="M57.33333,28.66667c-7.91917,0 -14.33333,6.41417 -14.33333,14.33333v86c0,7.91917 6.41417,14.33333 14.33333,14.33333c7.91917,0 14.33333,-6.41417 14.33333,-14.33333v-86c0,-7.91917 -6.41417,-14.33333 -14.33333,-14.33333z"
+          opacity="0.35"
+        />
+        <rect
+          x="-18"
+          y="14"
+          transform="rotate(-90) scale(7.16667,7.16667)"
+          width="12"
+          height="4"
+        />
+        <circle
+          cx="16"
+          cy="6"
+          transform="scale(7.16667,7.16667)"
+          r="2"
+        />
+        <circle
+          cx="16"
+          cy="18"
+          transform="scale(7.16667,7.16667)"
+          r="2"
+        />
+      </g>
+    </g>
+  </svg>
+</template>
---
+++ src/renderer/icons/plumpy/index.ts
@@ -0,0 +1,15 @@
+import NextIcon from "./NextIcon.vue";
+import PauseIcon from "./PauseIcon.vue";
+import PlayIcon from "./PlayIcon.vue";
+import RepeatIcon from "./RepeatIcon.vue";
+import RepeatOneIcon from "./RepeatOneIcon.vue";
+import ShuffleIcon from "./ShuffleIcon.vue";
+
+export {
+  NextIcon,
+  PauseIcon,
+  PlayIcon,
+  RepeatIcon,
+  RepeatOneIcon,
+  ShuffleIcon,
+};
