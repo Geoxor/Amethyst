b6f6d014718b4a8bc3718ca7740aa41022533ded
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2022-12-13 20:36:14 +0200

    added home page & fixed header colors

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,11 +4,15 @@ export default defineConfig({
   title: "Amethyst",
   description: "Amethyst Documentation",
   lang: 'en-US',
+  
   head: [
     ["link", {rel: "icon", href: "https://github.com/Geoxor/amethyst/raw/master/assets/icon.svg"}]
   ],
   appearance: "dark",
   themeConfig: {
+    footer: {
+      message: "Made with the loss of multiple braincells 🧠"
+    },
     socialLinks: [
       {
         link: "https://discord.gg/geoxor",
@@ -24,6 +28,10 @@ export default defineConfig({
     },
     siteTitle: "Amethyst",
     logo: "https://raw.githubusercontent.com/Geoxor/amethyst/master/assets/icon.svg",
+    nav: [
+      { text: "Documentation", link: "/introduction" },
+      { text: "Download", link: "/installation/package_managers"},
+    ],
     sidebar: [
       {
         text: 'Installation',
