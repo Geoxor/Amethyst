6696f9238b8998508862dc5be0bd52dd0baedf82
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2022-12-27 18:37:54 +0200

    move window types

--- src/renderer/types/window.d.ts
+++ src/renderer/types/window.d.ts
@@ -1,3 +1,5 @@
+import type { Channels } from "../../main/preload";
+
 declare type fs = typeof import("fs/promises");
 declare type os = typeof import("os");
 declare type path = typeof import("path");
@@ -8,6 +10,17 @@ declare global {
     os: os;
     path: path;
     dialog: Electron.Dialog;
+    electron: {
+			ipcRenderer: {
+				invoke<T>(channel: Channels, args?: any[]): Promise<T>
+				send(channel: Channels, args: unknown[]): void
+				on<T>(
+					channel: string,
+					func: (...args: T[]) => void
+				): (() => void) | undefined
+				once<T>(channel: string, func: (...args: T[]) => void): void
+			}
+		}
   }
 }
 
