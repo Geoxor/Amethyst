c2f3c3d543cfd5c6a7036c6f34dd6e9924e30120
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2023-01-25 12:00:26 +0200

    reset on right click

--- src/renderer/views/QueueView.vue
+++ src/renderer/views/QueueView.vue
@@ -29,6 +29,7 @@ onMounted(() => {
 onUnmounted(() => {
   player.off("play", autoscroll);
 });
+
 </script>
 
 <template>
@@ -39,6 +40,7 @@ onUnmounted(() => {
       class="border-1 z-30 select-none w-full bg-surface-800 border-surface-600 text-white py-0.25 placeholder-primary-900 placeholder-opacity-75 hover:placeholder-opacity-100 indent-xs text-12px mb-2"
       placeholder="name, album & artist..."
       @keydown.stop
+      @mousedown="$event.which == 3 && (filterText = '')"
     >
 
     <square-button
