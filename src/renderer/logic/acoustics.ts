e4ce20540b03d523a788191b9e9ff4f3c92f75eb
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2023-01-31 01:09:02 +0200

    more functions for acoustic simulations

--- src/renderer/logic/acoustics.ts
+++ src/renderer/logic/acoustics.ts
@@ -1,10 +1,22 @@
 const RT60_CONSTANT = 0.161;
-const SPEED_OF_SOUND_IN_AIR_AT_25_DEGREES_CELCIUS = 346;
+// 0°C in Kelvin, represents the difference between the Celsius and Kelvin temperature scales.
+const ABSOLUTE_ZERO = 273.15;
+// Speed of sound in air at 20°C (68°F)
+const SPEED_OF_SOUND = 340.29;
 
 export interface RoomDimensions {
   width: number;
-  length: number; 
+  length: number;
   height: number;
+}
+
+export function calculateSpeedOfSound(temperature: number) {
+  // Speed of sound in air varies with temperature
+  // Reference: https://en.wikipedia.org/wiki/Speed_of_sound#Temperature
+
+  // The 273.15 in the formula Math.sqrt(1 + (temperature - 20) / 273.15) is the difference 
+  // between the reference temperature of 20°C (68°F) and the absolute zero temperature in Kelvin (K).
+  return SPEED_OF_SOUND * Math.sqrt(1 + (temperature - 20) / ABSOLUTE_ZERO);
 }
 
 // The volume parameter is the volume of the room in cubic meters and the absorptionCoefficient 
@@ -34,12 +46,13 @@ export const calculateRT60ByDimensions = ({width, length, height}: RoomDimension
 // for i, j, k and the frequency of the mode. The frequency values are in hertz (Hz).
 
 // The number of modes to calculate is specified by the modeCount parameter.
+// The 2 in the formula is a mathematical constant that represents the number of positive and negative cycles in a wave.
 export const calculateRoomModes = ({width, length, height}: RoomDimensions, modeCount = 50) => {
   const modes = [];
   for (let i = 1; i <= modeCount; i++) {
     for (let j = 1; j <= modeCount; j++) {
       for (let k = 1; k <= modeCount; k++) {
-        const frequency = (2 * i * j * k * SPEED_OF_SOUND_IN_AIR_AT_25_DEGREES_CELCIUS) / (width * length * height);
+        const frequency = (2 * i * j * k * calculateSpeedOfSound(20)) / (width * length * height);
         modes.push({ i, j, k, frequency });
       }
     }
