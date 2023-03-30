967bced74b2997aa780a48f344ac025fbfe24e6d
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2023-03-30 14:49:30 +0300

    fix tests

--- src/renderer/components/input/BaseSlider.test.ts
+++ src/renderer/components/input/BaseSlider.test.ts
@@ -10,12 +10,12 @@ describe.concurrent("Slider.vue", () => {
 
   it("should have a value of 50", () => {
     const mounted = mount(Slider, { props: { modelValue: 50 } });
-    expect(((mounted.element as HTMLInputElement).value)).toEqual(50);
+    expect(((mounted.element as HTMLInputElement).value)).toEqual("50");
   });
 
   it("should emit v-model update when changing value", () => {
     const mounted = mount(Slider, { props: { modelValue: 50 } });
-    expect(((mounted.element as HTMLInputElement).value)).toEqual(50);
+    expect(((mounted.element as HTMLInputElement).value)).toEqual("50");
     mounted.setValue(25);
     expect(mounted.emitted()["update:modelValue"]).toBeTruthy();
   });
