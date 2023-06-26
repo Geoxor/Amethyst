29d1a4472cfc4cdeb9d26d0a4371c4a1dd9f2d49
otiskujawa <49094075+otiskujawa@users.noreply.github.com>
2023-06-26 16:48:40 +0200

    Change number of bars in SpectrumAnalyzer.vue

--- src/renderer/components/visualizers/SpectrumAnalyzer/SpectrumAnalyzer.vue
+++ src/renderer/components/visualizers/SpectrumAnalyzer/SpectrumAnalyzer.vue
@@ -50,7 +50,7 @@ onMounted(async () => {
   return vertices;
 };
 
-  const TOTAL_BARS = 1024;
+  const TOTAL_BARS = 1000;
 
   const loader = new THREE.FileLoader();
   const camera = new THREE.OrthographicCamera(0, width, -height * 1.25 , 0, 0, 10000 );
