d238d69f5328845aaca0ee931e88e579f10c83f5
Geoxor <34042825+Geoxor@users.noreply.github.com>
2023-12-10 20:47:40 +0200

    add new icons

---
+++ src/renderer/icons/ExportIcon.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      opacity="0.35"
+      d="M17 22H7C5.343 22 4 20.657 4 19V5C4 3.343 5.343 2 7 2H17C18.657 2 20 3.343 20 5V19C20 20.657 18.657 22 17 22Z"
+      fill="currentColor"
+    />
+    <path
+      d="M19 10H12C10.895 10 10 10.895 10 12C10 13.105 10.895 14 12 14H19V10Z"
+      fill="currentColor"
+    />
+    <path
+      d="M18 15.3052C18 15.9222 18.744 16.2322 19.182 15.7982L22.296 12.7132C22.692 12.3202 22.692 11.6802 22.296 11.2872L19.182 8.2022C18.744 7.7682 18 8.0792 18 8.6952V15.3052Z"
+      fill="currentColor"
+    />
+  </svg>
+</template>
