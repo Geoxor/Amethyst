30d377e098fe4f96153c51919c9115a25558b1e3
Geoxor <34042825+Geoxor@users.noreply.github.com>
2023-12-11 05:35:39 +0200

    Update BasePopover.vue

--- src/renderer/components/BasePopover.vue
+++ src/renderer/components/BasePopover.vue
@@ -64,7 +64,7 @@ onMounted(async() => {
   <div @mouseenter="updatePosition()">
     <slot />
   </div>
-  <!-- TODO: refactor this to use watch and a css variable instead like duration-adaptive -->
+  <!-- TODO: refactor this to use watch and a css variable instead like duration-user-defined -->
   <div
     ref="popover"
     :style="`transition-duration: ${state.settings.value.animationDuration}ms`"
