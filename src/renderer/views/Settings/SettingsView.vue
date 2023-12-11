45ceab4aab03dafcf52901cec78f897ef9f3fa76
Geoxor <34042825+Geoxor@users.noreply.github.com>
2023-12-11 05:28:53 +0200

    proper casing for this

--- src/renderer/views/Settings/SettingsView.vue
+++ src/renderer/views/Settings/SettingsView.vue
@@ -4,7 +4,7 @@ import SettingsNavigation from "@/components/v2/SettingsNavigation.vue";
 
 <template>
   <div class="h-full flex w-ful p-2">
-    <SettingsNavigation />
+    <settings-navigation />
     <div class="flex flex-col px-2 gap-2 w-full h-full overflow-y-auto">
       <router-view />
     </div>
