90f4c5292a9aa1aba307eb4d0e395627c5738987
Scanavium21 <SteamDiscord211@gmx.net>
2023-12-15 16:20:15 +0100

    added basic methods

--- src/renderer/logic/playlist.ts
+++ src/renderer/logic/playlist.ts
@@ -1,13 +1,27 @@
+import { PathLike } from "fs";
 import { Track } from "./track";
+import { ref } from "vue";
 
 export class Playlist{
-    public list: Track[];
+    public list = new Array<Track>;
+    public name = ref(new String("Playlist"));
 
     public constructor(tracks?: Track[]){
-        tracks ? this.list = Array.from(tracks) : this.list = new Array<Track>;
+        tracks && tracks.forEach(track => this.list.push(track));
     }
 
     public getTracks(): Track[]{
         return this.list;
+    }
+
+    public async loadTracks(){
+
+    }
+
+    /**
+     * 
+     */
+    public async loadFromFile(path: PathLike){
+
     }
 }
