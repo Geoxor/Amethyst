78453ed58ad5c35d2c573fbf920f542255a4ba37
Geoxor <34042825+Geoxor@users.noreply.github.com>
2023-12-15 20:38:24 +0200

    update pause icon too

--- src/renderer/icons/playback/PauseIcon.vue
+++ src/renderer/icons/playback/PauseIcon.vue
@@ -3,25 +3,24 @@
     width="24"
     height="24"
     viewBox="0 0 24 24"
-    fill="currentColor"
-    version="1.1"
+    fill="none"
     xmlns="http://www.w3.org/2000/svg"
-    xmlns:svg="http://www.w3.org/2000/svg"
   >
     <path
-      id="path1"
-      style="fill:currentColor"
-      d="M 7,6 C 7,5 7.5,5 8,5 8.5,5 9,5 9,6 9,7 9,17 9,18 9,19 8.5,19 8,19 7.5,19 7,19 7,18 7,17 7,6 7,6 Z"
+      d="M8 4C6.895 4 6 4.895 6 6V18C6 19.105 6.895 20 8 20C9.105 20 10 19.105 10 18V6C10 4.895 9.105 4 8 4Z"
+      fill="currentColor"
     />
     <path
-      id="path2"
-      style="fill:currentColor"
-      d="M 7,6 C 7,5 7.5,5 8,5 8.5,5 9,5 9,6 9,7 9,17 9,18 9,19 8.5,19 8,19 7.5,19 7,19 7,18 7,17 7,6 7,6 Z"
+      d="M14 6V18H18V6H14Z"
+      fill="currentColor"
     />
     <path
-      id="path2-0"
-      style="fill:currentColor"
-      d="m 15,6 c 0,-1 0.5,-1 1,-1 0.5,0 1,0 1,1 0,1 0,11 0,12 0,1 -0.5,1 -1,1 -0.5,0 -1,0 -1,-1 0,-1 0,-12 0,-12 z"
+      d="M16 8C17.1046 8 18 7.10457 18 6C18 4.89543 17.1046 4 16 4C14.8954 4 14 4.89543 14 6C14 7.10457 14.8954 8 16 8Z"
+      fill="currentColor"
+    />
+    <path
+      d="M16 20C17.1046 20 18 19.1046 18 18C18 16.8954 17.1046 16 16 16C14.8954 16 14 16.8954 14 18C14 19.1046 14.8954 20 16 20Z"
+      fill="currentColor"
     />
   </svg>
 </template>
