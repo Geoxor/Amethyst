c4464d79f30ff697853660c419e26651c6b787fe
nyabsi <nyabsi@sovellus.cc>
2023-12-17 12:43:23 +0200

    Add rust changes to reflect new source behaviour

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -11,25 +11,22 @@ use std::sync::{Arc, Mutex};
 use tauri::api::dialog::FileDialogBuilder;
 use std::path::PathBuf;
 
-#[derive(Clone, serde::Serialize)]
-struct FilePayload {
-  files: Vec<PathBuf>,
-}
-
-#[derive(Clone, serde::Serialize)]
-struct FolderPayload {
-  folder: PathBuf
-}
-
 lazy_static! {
-  static ref client: Mutex<DiscordIpcClient> = Mutex::new({
+  static ref CLIENT: Mutex<DiscordIpcClient> = Mutex::new({
     let c = DiscordIpcClient::new("976036303156162570").unwrap();
     c
   });
 }
 
 #[tauri::command]
-fn update_presence(title: String, time: String, format: String) {
+async fn pick_folder() -> (bool, PathBuf) {
+  use tauri::api::dialog::blocking::FileDialogBuilder;
+  let path = FileDialogBuilder::new().pick_folder().unwrap();
+  return (path.is_dir(), path);
+}
+
+#[tauri::command]
+async fn update_presence(title: String, time: String, format: String) {
   
   let assets = activity::Assets::new()
   .large_image("audio_file")
@@ -43,17 +40,17 @@ fn update_presence(title: String, time: String, format: String) {
     .details(&title)
     .assets(assets);
   
-  let _ = client.lock().unwrap().set_activity(payload).unwrap();
+  let _ = CLIENT.lock().unwrap().set_activity(payload);
 }
 
 fn main() {
 
-  let _ = client.lock().unwrap().connect().unwrap();
+  let _ = CLIENT.lock().unwrap().connect();
 
   #[cfg(debug_assertions)]
   let menu = Menu::new()
     .add_submenu(Submenu::new("Amethyst", Menu::new().add_native_item(MenuItem::About("Amethyst".to_string(), AboutMetadata::new())).add_native_item(MenuItem::Separator).add_native_item(MenuItem::Quit)))
-    .add_submenu(Submenu::new("File", Menu::new().add_item(CustomMenuItem::new("open_file".to_string(), "Open File").accelerator("Command+O")).add_item(CustomMenuItem::new("open_folder".to_string(), "Open Folder").accelerator("Command+Shift+O"))))
+    .add_submenu(Submenu::new("File", Menu::new().add_item(CustomMenuItem::new("add_source".to_string(), "Add Source").accelerator("Command+O"))))
     .add_submenu(Submenu::new("Utility", Menu::new().add_item(CustomMenuItem::new("clear_queue".to_string(), "Clear queue").accelerator("Command+Shift+X")).add_item(CustomMenuItem::new("clear_invalid".to_string(), "Clear errored / deleted").accelerator("Command+Shift+Z")).add_native_item(MenuItem::Separator).add_item(CustomMenuItem::new("refresh_metadata".to_string(), "Refresh all metadata").accelerator("Command+Alt+R")).add_item(CustomMenuItem::new("refresh_window".to_string(), "Refresh window").accelerator("Command+R"))))
     .add_submenu(Submenu::new("View", Menu::new().add_item(CustomMenuItem::new("settings".to_string(), "Settings").accelerator("Command+S"))))
     .add_submenu(Submenu::new("Window", Menu::new().add_native_item(MenuItem::Minimize).add_native_item(MenuItem::Zoom).add_native_item(MenuItem::CloseWindow).add_native_item(MenuItem::Separator).add_native_item(MenuItem::ShowAll)))
@@ -70,44 +67,12 @@ fn main() {
     .add_submenu(Submenu::new("About", Menu::new().add_item(CustomMenuItem::new("documentation".to_string(), "Go to Documentation")).add_item(CustomMenuItem::new("github".to_string(), "Go to Github")).add_item(CustomMenuItem::new("discord".to_string(), "Join Discord Server"))));
 
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![update_presence])
+    .invoke_handler(tauri::generate_handler![update_presence, pick_folder])
     .menu(menu)
     .on_menu_event(|event| {
       match event.menu_item_id() {
-        "open_file" => {
-          FileDialogBuilder::new().add_filter("Music", &[
-            "ogg", 
-            "ogv", 
-            "oga", 
-            "ogx", 
-            "ogm", 
-            "spx", 
-            "opus", 
-            "wav", 
-            "wave", 
-            "m4a", 
-            "m4b", 
-            "m4p", 
-            "m4r", 
-            "m4v", 
-            "3gp", 
-            "flac", 
-            "opus", 
-            "aac", 
-            "aiff", 
-            "mp3", 
-            "m4a",
-            "mp4",
-            "dfpwm",
-            "webm",
-          ]).pick_files(move |file_paths| {
-            event.window().emit("open-file", FilePayload { files: file_paths.unwrap() }).unwrap();
-          })
-        },
-        "open_folder" => {
-          FileDialogBuilder::new().pick_folder(move |folder_path| {
-            event.window().emit("open-folder", FolderPayload { folder: folder_path.unwrap() }).unwrap();
-          })
+        "add_source" => {
+          event.window().emit("add-source", {}).unwrap();
         },
         "settings" => {
           event.window().emit("goto-settings", {}).unwrap();
