54c2a5278467ac91ace7f2e0c91505c744b1f10f
nyabsi <nyabsi@sovellus.cc>
2023-12-17 12:37:38 +0200

    Implement Tauri support for mediaSources and fix circular dependecy

--- src/renderer/logic/mediaSources.ts
+++ src/renderer/logic/mediaSources.ts
@@ -3,6 +3,9 @@ import { Track } from "./track";
 import { Ref, ref } from "vue";
 import { Player } from "./player";
 import { Store } from "@/state";
+import { invoke } from '@tauri-apps/api/tauri';
+import { tauriUtils } from "@/tauri-utils";
+import { Amethyst } from "@/amethyst";
 
 export enum MediaSourceType {
   LocalFolder = "settings.media_source_type.local_folder",
@@ -11,31 +14,52 @@ export enum MediaSourceType {
 
 export class MediaSourceManager {
   public mediaSources = ref<MediaSource[]>([]);
+  private readonly amethyst: Amethyst;
  
-  public constructor(protected player: Player, protected store: Store) {
+  public constructor(protected player: Player, protected store: Store, amethyst: Amethyst) {
+    this.amethyst = amethyst;
     this.store.settings.value.saveMediaSources.forEach(savedSource => {
       if (savedSource.path) {
-        this.mediaSources.value.push(new LocalMediaSource(this.player, this.store, savedSource.path));
+        this.mediaSources.value.push(new LocalMediaSource(this.player, this.store, savedSource.path, amethyst));
       }
     });
   }
 
   public addLocalSource = async () => {
-    // dynamic import to avoid circular dependency causing a paradox
-    // TODO: fix this ^ maybe move all the FS related shit into its own file
-    const dialog = await (await import("@/amethyst")).amethyst.showOpenFolderDialog();
-    const path = dialog.filePaths[0];
-  
-    if (dialog.canceled || !path) return;
-  
-    // Avoid adding folders if they already exist
-    if (this.store.settings.value.saveMediaSources.some(savedSource => savedSource.path == path)) return;
+    if (this.amethyst.isUsingTauri())
+    {
+      const dialog = await invoke('pick_folder', {});
 
-    const mediaSource = new LocalMediaSource(this.player, this.store, path);
+      if (!dialog[0]) return;
+
+      const path = dialog[1];
+      
+      // Avoid adding folders if they already exist
+      if (this.store.settings.value.saveMediaSources.some(savedSource => savedSource.path == path)) return;
+
+      const mediaSource = new LocalMediaSource(this.player, this.store, path, this.amethyst);
+      
+      if (mediaSource.type && mediaSource.path) {
+        this.store.settings.value.saveMediaSources.push({type: mediaSource.type, path: mediaSource.path});
+        this.mediaSources.value.push(mediaSource);
+      }
+    }
+    else
+    {
+      const dialog = await this.amethyst.showOpenFolderDialog();
+      const path = dialog.filePaths[0];
     
-    if (mediaSource.type && mediaSource.path) {
-      this.store.settings.value.saveMediaSources.push({type: mediaSource.type, path: mediaSource.path});
-      this.mediaSources.value.push(mediaSource);
+      if (dialog.canceled || !path) return;
+    
+      // Avoid adding folders if they already exist
+      if (this.store.settings.value.saveMediaSources.some(savedSource => savedSource.path == path)) return;
+
+      const mediaSource = new LocalMediaSource(this.player, this.store, path, this.amethyst);
+      
+      if (mediaSource.type && mediaSource.path) {
+        this.store.settings.value.saveMediaSources.push({type: mediaSource.type, path: mediaSource.path});
+        this.mediaSources.value.push(mediaSource);
+      }
     }
   };
   
@@ -54,27 +78,43 @@ export class MediaSource {
   public name: string = "generic";
   public tracks: Track[] = [];
 
-  public constructor(protected player: Player, protected store: Store, public path: string) {
+  private readonly amethyst: Amethyst;
+
+  public constructor(protected player: Player, protected store: Store, public path: string, amethyst: Amethyst) {
+    this.amethyst = amethyst;
     this.fetchMedia();
   }
 
   private async fetchMedia() {
-    const paths = await window.electron.ipcRenderer.invoke<string[]>("fetch-folder-content", [this.path, [{name: "Audio", extensions: ALLOWED_AUDIO_EXTENSIONS}]]);
-    const audioFiles = paths.filter(file => ALLOWED_AUDIO_EXTENSIONS.some(ext => file.endsWith(ext)));
-    this.totalTracks.value = audioFiles.length;
-    this.tracks = audioFiles.map(path => new Track(path));
-    
-    // TODO: temporarily add tracks to the queue till theres discovery view added
-    this.tracks.forEach(track => this.player.queue.add(track));
-    await this.player.queue.fetchAsyncData();
+    if (await (await import("@/amethyst")).amethyst.isUsingTauri())
+    {
+      const path = await tauriUtils.tauriReadFolder(this.path);
+      const audioFiles = path.filter(file => ALLOWED_AUDIO_EXTENSIONS.some(ext => file.path.endsWith(ext)));
+
+      this.totalTracks.value = audioFiles.length;
+      this.tracks = audioFiles.map(file => new Track(file.path));
+      
+      // TODO: temporarily add tracks to the queue till theres discovery view added
+      this.tracks.forEach(track => this.player.queue.add(track));
+      await this.player.queue.fetchAsyncData();
+    } else
+    {
+      const paths = await window.electron.ipcRenderer.invoke<string[]>("fetch-folder-content", [this.path, [{name: "Audio", extensions: ALLOWED_AUDIO_EXTENSIONS}]]);
+      const audioFiles = paths.filter(file => ALLOWED_AUDIO_EXTENSIONS.some(ext => file.endsWith(ext)));
+      this.totalTracks.value = audioFiles.length;
+      this.tracks = audioFiles.map(path => new Track(path));
+      
+      // TODO: temporarily add tracks to the queue till theres discovery view added
+      this.tracks.forEach(track => this.player.queue.add(track));
+      await this.player.queue.fetchAsyncData();
+    }
   }
 }
 
 export class LocalMediaSource extends MediaSource {
-  public constructor(protected player: Player, protected store: Store, public path: string) {
-    super(player, store, path);
-
+  public constructor(protected player: Player, protected store: Store, public path: string, amethyst: Amethyst) {
+    super(player, store, path, amethyst);
     this.type = MediaSourceType.LocalFolder;
-    this.name = window.path.parse(this.path).base;
+    this.name = amethyst.isUsingTauri() ? tauriUtils.tauriGetRootDirectory(this.path) : window.path.parse(this.path).base;;
   }
 }
