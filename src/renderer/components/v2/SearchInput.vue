fe9aa5225fc820bf53e9f7c1608a7543394bf9d5
Geoxor <34042825+Geoxor@users.noreply.github.com>
2023-12-17 04:23:51 +0200

    localize

--- src/renderer/components/v2/SearchInput.vue
+++ src/renderer/components/v2/SearchInput.vue
@@ -12,7 +12,7 @@ const value = useVModel(props, "modelValue", emits);
     v-model="value"
     type="text"
     class="text-10px placeholder-text_subtitle text-text_title py-2 px-4 bg-surface-700 w-44 rounded-8px font-semibold"
-    placeholder="Search"
+    :placeholder="$t('search.placeholder')"
     @keydown.stop
     @mousedown="$event.which == 3 && (value = '')"
   >
