666031487518dc063a2410610ba2807bf766c73c
nyabsi <nyabsi@sovellus.cc>
2023-12-18 02:31:54 +0200

    It's "macos" not "mac_os" (fix target_os)

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -61,7 +61,7 @@ fn main() {
   let _ = CLIENT.lock().unwrap().connect();
 
   #[cfg(debug_assertions)]
-  #[cfg(target_os = "mac_os")]
+  #[cfg(target_os = "macos")]
   let menu = Menu::new()
     .add_submenu(Submenu::new("Amethyst", Menu::new().add_native_item(MenuItem::About("Amethyst".to_string(), AboutMetadata::new())).add_native_item(MenuItem::Separator).add_native_item(MenuItem::Quit)))
     .add_submenu(Submenu::new("File", Menu::new().add_item(CustomMenuItem::new("add_source".to_string(), "Add Source").accelerator("Command+O"))))
@@ -72,7 +72,7 @@ fn main() {
     .add_submenu(Submenu::new("About", Menu::new().add_item(CustomMenuItem::new("documentation".to_string(), "Go to Documentation")).add_item(CustomMenuItem::new("github".to_string(), "Go to Github")).add_item(CustomMenuItem::new("discord".to_string(), "Join Discord Server"))));
 
   #[cfg(not(debug_assertions))]
-  #[cfg(target_os = "mac_os")]
+  #[cfg(target_os = "macos")]
   let menu = Menu::new()
     .add_submenu(Submenu::new("Amethyst", Menu::new().add_native_item(MenuItem::About("Amethyst".to_string(), AboutMetadata::new())).add_native_item(MenuItem::Separator).add_native_item(MenuItem::Quit)))
     .add_submenu(Submenu::new("File", Menu::new().add_item(CustomMenuItem::new("open_file".to_string(), "Open File").accelerator("Command+O")).add_item(CustomMenuItem::new("open_folder".to_string(), "Open Folder").accelerator("Command+Shift+O"))))
