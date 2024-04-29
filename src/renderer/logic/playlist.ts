81a8284262a65ee7b7e7e536ae7e8947bc976760
Scanavium21 <Scanavium21@Protonmail.com>
2024-04-29 16:18:55 +0200

    Added basic playlist write implementation

--- src/renderer/logic/playlist.ts
+++ src/renderer/logic/playlist.ts
@@ -1,6 +1,7 @@
 import { PathLike } from "fs";
 import { Track } from "./track";
 import { FileIO } from "./io/FileIO";
+import { PlaylistFileType } from "./io/Playlists";
 import { AMETHYST_PLAYLIST_EXTENSION } from "@shared/constants";
 import { amethyst } from "@/amethyst";
 
@@ -28,22 +29,30 @@ export class Playlist {
      * If path is directory, it will save the playlist in the custom Amethyst format and the playlist's name in the given directory
      * Returns false if unable to save to file.
      * @param path the path where the file will be stored including the extension if valid
-     * @param overwrite if true, it will replace any existing file with the same name
      * @param fallback if true, any invalid extension will fall back to the Amethyst playlist extension
      * @returns true if successful, false if: Track count < 1, invalid/nonexisting/undefined path/extension or due to an IO error
      */
-    public saveToFile(path?: PathLike, overwrite: boolean = false, fallback: boolean = true): boolean {
+    public saveToFile(path: PathLike, fallback: boolean = true) {
         let exportPath = (path) ? path : window.path.join(amethyst.APPDATA_PATH || "", "/amethyst/Playlists", this.name, ".", AMETHYST_PLAYLIST_EXTENSION);
         if (path && !FileIO.extensionAllowed(path) && fallback) {
             exportPath = FileIO.replaceExtension(path, AMETHYST_PLAYLIST_EXTENSION);
         }
-        return FileIO.writePlaylist(this, exportPath, overwrite);
+        const extension = FileIO.getExtension(exportPath);
+        if(!extension || !FileIO.extensionAllowed(extension)) {
+            throw new ReferenceError("Invalid or non-exsiting file extension");
+        }
+        const writer = PlaylistFileType.forExtension(extension);
+        if(!writer) {
+            throw new ReferenceError("File extension is not mapped");
+        }
+        const data = writer.pack(this);
+        FileIO.writeFile(exportPath, data);
     }
 
     /**
      * 
      */
-    public async loadFromFile(path: PathLike) {
-
+    public static loadFromFile(path: PathLike): Playlist | undefined {
+        return undefined; // TODO:
     }
 }
