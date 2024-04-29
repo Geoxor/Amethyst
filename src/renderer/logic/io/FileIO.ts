427bf847f0f83f3dabd4dd395ac2641452df75c6
Scanavium21 <Scanavium21@Protonmail.com>
2024-04-29 15:52:06 +0200

    Removed redundant methods

--- src/renderer/logic/io/FileIO.ts
+++ src/renderer/logic/io/FileIO.ts
@@ -1,30 +1,9 @@
 import { PathLike } from "fs";
-import { Playlist } from "../playlist";
 import { ALLOWED_AUDIO_EXTENSIONS, ALLOWED_EXTENSIONS, ALLOWED_PLAYLIST_FORMATS, AMETHYST_PLAYLIST_EXTENSION } from "@shared/constants";
 import { amethyst } from "@/amethyst";
 
 /** A simple class that contains functions for paths, file input and output */
 export abstract class FileIO {
-
-    /**
-     * This function stores a playlist to a file
-     * @param playlist the playlist that should be stored
-     * @param path the path where it should be stored at, if invalid, it will use the default path
-     * @param overwrite if true, it will overwrite the file with the same name, if present
-     * @returns if successful true, false otherwise
-     */
-    public static writePlaylist(playlist: Playlist, path: PathLike, overwrite: boolean): boolean { // TODO:
-        return false;
-    }
-
-    /**
-     * Returns a playlist stored as a file, or undefined if invalid
-     * @param path the path where the a playlist is located
-     * @returns a playlist instance if successful, undefined otherwise
-     */
-    public static readPlaylist(path: PathLike): Playlist | undefined { // TODO:
-        return undefined;
-    }
 
     public static async writeFile(path: PathLike, data: String) {
         window.fs.writeFile(path, data).catch(error => undefined);
