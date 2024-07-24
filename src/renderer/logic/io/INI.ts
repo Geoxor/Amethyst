b2efc469c45e8941eee6f70c2c15a56aec412383
Scanavium21 <Scanavium21@Protonmail.com>
2024-07-24 07:57:44 +0200

    Added basic ini parser

--- src/renderer/logic/io/INI.ts
+++ src/renderer/logic/io/INI.ts
@@ -1,14 +1,9 @@
-import fs from "fs";
-import { PathLike } from "fs";
-
 /** A class used to handle files formatted in INI */
 export class Entry {
-    public section?: string;
-    public key?: string;
-    public value?: string;
+    public key: string;
+    public value: string;
 
-    public Entry(section?: string, key?: string, value?: string) {
-        this.section = section;
+    public constructor(key: string, value: string) {
         this.key = key;
         this.value = value;
     }
@@ -17,35 +12,42 @@ export class Entry {
 export class INI {
 
     private static readonly SECTION_LIMITER: string[] = ["[", "]"];
+    private static readonly COMMENT: string = "#";
     private static readonly KEY_VALUE_SEPARATOR: string = "=";
 
-    public static readSection(path: PathLike, section: string): Entry[] | undefined {
-        const inputStream = fs.createReadStream(path);
-        const result = new Array<Entry>;
-        inputStream.forEach(char => {
-
-        });
-        return;
-    }
-
-    public static readFile(path: PathLike): Entry[] | undefined {
-        const inputStream = fs.createReadStream(path);
-        const result = new Array<Entry>;
-        let currentSection: string = "";
-        let cache: string = "";
-        inputStream.forEach(buffer => {
-            if(buffer == this.SECTION_LIMITER[0]) {
-                cache = "";
-            }else if(buffer == this.SECTION_LIMITER[1]) {
-                currentSection = cache;
-                cache = "";
-            }else {
-                
+    public static async parseINI(path: string): Promise<Map<string, Entry[]>> {
+        const input = await window.fs.readFile(path);
+        const result = new Map<string, Entry[]>;
+        let section: string = "";
+        const cache: Entry[] = new Array<Entry>();
+        let line: String = "";
+        
+        for (const character in input) {
+            if(character != "\n") {
+                line = line.concat(character);
+            } else {
+                line = line.trim().split(this.COMMENT)[0];
+                if(line == "") continue;
+                if(line.startsWith(this.SECTION_LIMITER[0]) && line.endsWith(this.SECTION_LIMITER[1])) {
+                    if(section != "" && cache.length > 0) {
+                        result.set(section, cache);
+                    }
+                    section = line.substring(this.SECTION_LIMITER[0].length, line.length - this.SECTION_LIMITER[1].length);
+                    if(section == "") {
+                        throw new ReferenceError("Section name is empty");
+                    }
+                } else {
+                    if(section == "") {
+                        throw new ReferenceError("Line is not part of a section");
+                    }
+                    const entry: Entry = new Entry(line.substring(0, line.indexOf(this.KEY_VALUE_SEPARATOR)), line.substring(line.indexOf(this.KEY_VALUE_SEPARATOR) + 1));
+                    if(entry.key == "") throw new ReferenceError("Line does not contain a valid key");
+                    cache.push(entry);
+                }
             }
-            cache = cache.concat(buffer);
-        }).finally(() => {
-            inputStream.close();
+        }
+        return new Promise<Map<string, Entry[]>>(resolve => {
+            resolve(result);
         });
-        return;
     }
 }
