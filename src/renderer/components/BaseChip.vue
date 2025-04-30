79b0b1f13450901613405a4e4c0f311e5d68c08d
Geoxor <34042825+Geoxor@users.noreply.github.com>
2025-04-30 06:17:38 +0300

    looks better like this

--- src/renderer/components/BaseChip.vue
+++ src/renderer/components/BaseChip.vue
@@ -4,7 +4,7 @@ defineProps<{ icon?: any, color?: string }>();
 
 <template>
   <div
-    class="select-none min-w-20 justify-center text-11px font-weight-user-defined flex items-center leading-tight gap-1 rounded-full py-1 px-2"
+    class="select-none min-w-min justify-center text-11px font-weight-user-defined flex items-center leading-tight gap-1 rounded-full py-1 px-2"
     :class="color ? `bg-${color} bg-opacity-15 text-${color}` : 'bg-primary text-primary bg-opacity-15'"
   >
     <component
