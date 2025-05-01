a74624bfa3efc37e07116f26808675ee7c30e64c
Scanavium21 <Scanavium21@Protonmail.com>
2025-05-01 16:08:45 +0200

    Use ShaderCanvas in BackgroundImage component

--- src/renderer/components/BackgroundImage.vue
+++ src/renderer/components/BackgroundImage.vue
@@ -1,95 +1,55 @@
 <script setup lang="ts">
-import {amethyst} from "@/amethyst";
+import { watch } from "vue";
+import { amethyst } from "@/amethyst";
 import CoverArt from "@/components/CoverArt.vue";
-import * as THREE from "three";
-import {onMounted, onUnmounted, ref, watch} from "vue";
+import ShaderCanvas from "@/components/ShaderCanvas.vue";
 
 const props = defineProps<{
   ambientBackgroundImage?: string,
   node: AudioNode
 }>();
 
-// Somewhat copied from the SpectrumAnalyzer component
-// TODO: Maybe should make a dedicated shader component?
-const shaderCanvas = ref<HTMLCanvasElement>();
-let shouldStopRendering = false;
+// TODO: Maybe use a global analyser for all visuals (e.g. in player.ts)?
+const context = props.node.context;
+const analyser = context.createAnalyser();
 
-onMounted(() => {
-  if (!shaderCanvas.value) return;
+props.node.connect(analyser);
 
-  const scene = new THREE.Scene();
-  const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
-  const renderer = new THREE.WebGLRenderer({canvas: shaderCanvas.value, alpha: true});
+analyser.fftSize = amethyst.state.settings.value.spectrumFftSize;
+analyser.smoothingTimeConstant = amethyst.state.settings.value.spectrumSmoothing;
+watch(() => amethyst.state.settings.value.spectrumFftSize, () => analyser.fftSize = amethyst.state.settings.value.spectrumFftSize);
+watch(() => amethyst.state.settings.value.spectrumSmoothing, () => analyser.smoothingTimeConstant = amethyst.state.settings.value.spectrumSmoothing);
 
-  const geometry = new THREE.PlaneGeometry(2, 2);
-  const material = new THREE.ShaderMaterial({
-    uniforms: {
-      time: {value: 0}
-    },
-    vertexShader: `
-      void main() {
-        gl_Position = vec4(position, 1.0);
-      }
-    `,
-    fragmentShader: `
-      uniform float time;
-      void main() {
-        gl_FragColor = vec4(0.5 + 0.5 * sin(time + gl_FragCoord.x / 256.0), 0.0, 0.0, 1.0);
-      }
-    `
-  });
+// Don't change these
+analyser.maxDecibels = -0;
+analyser.minDecibels = -128;
 
-  const mesh = new THREE.Mesh(geometry, material);
-  scene.add(mesh);
+// TODO: Implement loading shaders from disk
+const fragmentShader = `
+  precision highp float;
+  uniform float u_time;
+  uniform vec2 u_resolution;
+  uniform float u_amplitudes[960];
+  void main() {
+    vec2 uv = gl_FragCoord.xy / u_resolution;
+    float amplitude = u_amplitudes[int(uv.x * 960.0)];
+    gl_FragColor = vec4(0.5 + 0.5 * sin(gl_FragCoord.x / 256.0 + u_time), 0, cos(uv.y + u_time), 1.0);
+  }`;
 
-  const animate = () => {
-    if (shouldStopRendering) return;
-    material.uniforms.time.value += 0.01;
-    renderer.render(scene, camera);
-  };
-
-  const context = props.node.context;
-  const analyser = context.createAnalyser();
-  const gain = context.createGain();
-
-  props.node.connect(gain);
-  gain.connect(analyser);
-
-  analyser.fftSize = amethyst.state.settings.value.spectrumFftSize;
-  analyser.smoothingTimeConstant = amethyst.state.settings.value.spectrumSmoothing;
-  watch(() => amethyst.state.settings.value.spectrumFftSize, () => analyser.fftSize = amethyst.state.settings.value.spectrumFftSize);
-  watch(() => amethyst.state.settings.value.spectrumSmoothing, () => analyser.smoothingTimeConstant = amethyst.state.settings.value.spectrumSmoothing);
-
-  // Don't change these
-  analyser.maxDecibels = -0;
-  analyser.minDecibels = -128;
-
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.setAnimationLoop(animate);
-});
-
-onUnmounted(() => {
-  shouldStopRendering = true;
-});
 </script>
 
 <template>
-  <div
+  <shader-canvas
     v-if="amethyst.state.settings.value.useShaderBackground"
-    class="fixed top-0 left-0 w-full h-full z-0"
     :style="`
-    mix-blend-mode: ${amethyst.state.settings.value.ambientBackgroundBlendMode};
-    `"
-  >
-    <canvas
-      ref="shaderCanvas"
-      class="w-full h-full"
-      :style="`
       opacity: ${amethyst.state.settings.value.ambientBackgroundOpacity}%;
       filter: blur(${amethyst.state.settings.value.ambientBackgroundBlurStrength}px);
-      `"
-    />
-  </div>
+      mix-blend-mode: ${amethyst.state.settings.value.ambientBackgroundBlendMode};
+    `"
+    :frag-shader="fragmentShader"
+    :analyser="analyser"
+    :pause-rendering="amethyst.state.settings.value.pauseVisualsWhenUnfocused && !amethyst.state.window.isFocused"
+  />
 
   <div
     v-else-if="ambientBackgroundImage"
@@ -114,7 +74,3 @@ onUnmounted(() => {
     />
   </div>
 </template>
-
-<style scoped lang="postcss">
-
-</style>
