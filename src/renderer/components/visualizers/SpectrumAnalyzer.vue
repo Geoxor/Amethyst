837e66e150b92d2ef6e028ec7e0bc21ea8b60fc8
Scanavium21 <Scanavium21@Protonmail.com>
2025-05-02 15:59:11 +0200

    Add basic spectrogram shader

--- src/renderer/components/visualizers/SpectrumAnalyzer.vue
+++ src/renderer/components/visualizers/SpectrumAnalyzer.vue
@@ -55,7 +55,31 @@ const SpectrumShader = `
     gl_FragColor = vec4(u_color * underCurve, underCurve);
   }`;
 
+const SpectrogramShader = `
+  precision highp float;
+
+  uniform vec2 u_resolution;
+  uniform sampler2D u_backbuffer;
+  uniform float[960] u_amplitudes;
+  uniform vec3 u_color;
+
+  void main(){
+    vec2 uv = gl_FragCoord.xy / u_resolution;
+    float amplitude = u_amplitudes[int(uv.y * float(u_amplitudes.length()))];
+    amplitude = clamp(pow(amplitude, 2.0), 0.0, 1.0);
+
+    // If first column
+    if (gl_FragCoord.x == 0.5) {
+      gl_FragColor = vec4(u_color - amplitude, 1.0);
+    } else {
+      // Get the color of the previous column (x-1)
+      gl_FragColor = texture2D(u_backbuffer, vec2(uv.x - (1.0 / u_resolution.x), uv.y));
+    }
+  }`;
+
 const shouldPause = () => amethyst.state.settings.value.pauseVisualsWhenUnfocused && !amethyst.state.window.isFocused;
+
+const getShader = () => amethyst.state.settings.value.useSpectrogram ? SpectrogramShader : SpectrumShader;
 
 </script>
 
@@ -63,7 +87,7 @@ const shouldPause = () => amethyst.state.settings.value.pauseVisualsWhenUnfocuse
   <div class="relative overflow-hidden w-full h-full rounded-4px">
     <shader-canvas
       class="origin-top-left absolute"
-      :frag-shader="SpectrumShader"
+      :frag-shader="getShader()"
       :analyser="analyser"
       :pause-rendering="shouldPause()"
       :uniforms="uniformData"
