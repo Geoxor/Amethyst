b072099a3c668065e7ec776ac8f9022c297962ea
Scanavium21 <Scanavium21@Protonmail.com>
2025-05-06 13:31:23 +0200

    Make spectrogram separate component

---
+++ src/renderer/components/visualizers/SpectrogramAnalyzer.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+import {logParabolicSpectrum, normalize8bit} from "@/logic/math";
+import ShaderCanvas from "@/components/ShaderCanvas.vue";
+import {VISUALIZER_BIN_COUNT} from "@shared/constants";
+import * as THREE from "three";
+import { watch } from "vue";
+import {SpectrogramShader} from "@/shaders/components/SpectrogramShader";
+import { amethyst } from "@/amethyst";
+import { getThemeColorRgb } from "@/logic/color";
+
+const props = defineProps<{
+  node: AudioNode,
+  accentColor: { r: number, g: number, b: number},
+  fftSize: number,
+  smoothing: number,
+  paused?: boolean,
+}>();
+
+const context = props.node.context;
+const analyser = context.createAnalyser();
+
+props.node.connect(analyser);
+
+const updateAnalyser = () => {
+  analyser.fftSize = props.fftSize;
+  analyser.smoothingTimeConstant = props.smoothing;
+};
+
+updateAnalyser();
+
+watch(() => [props.fftSize, props.smoothing], updateAnalyser);
+watch(() => amethyst.state.settings.value.theme, () => {
+  setTimeout(() => {
+    const accentColor = getThemeColorRgb("--accent");
+    uniformData.u_color.value.set(
+      normalize8bit(accentColor[0]),
+      normalize8bit(accentColor[1]),
+      normalize8bit(accentColor[2])
+    );
+  }, 100);
+});
+
+// Don't change these
+analyser.maxDecibels = -0;
+analyser.minDecibels = -128;
+
+const uniformData = {
+  u_color: {value: new THREE.Vector3(
+    normalize8bit(props.accentColor.r),
+    normalize8bit(props.accentColor.g),
+    normalize8bit(props.accentColor.b)
+  )},
+  u_amplitudes: {value: new Float32Array(VISUALIZER_BIN_COUNT)},
+};
+
+const render = (uniforms: Record<string, any>) => {
+  const spectrum = new Uint8Array(analyser.frequencyBinCount);
+  analyser.getByteFrequencyData(spectrum);
+  uniforms.u_amplitudes.value = logParabolicSpectrum(spectrum, VISUALIZER_BIN_COUNT);
+};
+</script>
+
+<template>
+  <div class="relative overflow-hidden w-full h-full rounded-4px">
+    <shader-canvas
+      class="origin-top-left absolute"
+      :frag-shader="SpectrogramShader"
+      :analyser="analyser"
+      :pause-rendering="paused"
+      :uniforms="uniformData"
+      @on-render="render"
+    />
+  </div>
+</template>
