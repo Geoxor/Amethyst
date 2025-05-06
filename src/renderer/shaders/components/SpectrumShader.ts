90740d56e865eef34e5df9e3b2aded6997ec70a6
Scanavium21 <Scanavium21@Protonmail.com>
2025-05-06 22:01:53 +0200

    Make spectrum shader scale thickness

--- src/renderer/shaders/components/SpectrumShader.ts
+++ src/renderer/shaders/components/SpectrumShader.ts
@@ -17,14 +17,14 @@ export const SpectrumShader = `
   void main(){
     vec2 uv = gl_FragCoord.xy / u_resolution;
     float amplitude = u_amplitudes[int(uv.x * float(u_amplitudes.length()))];
-    float left = u_amplitudes[int(uv.x * float(u_amplitudes.length())) - 1];
-    float right = u_amplitudes[int(uv.x * float(u_amplitudes.length())) + 1];
+    float left = u_amplitudes[int((uv.x - 1.0 / u_resolution.x) * float(u_amplitudes.length()))];
+    float right = u_amplitudes[int((uv.x + 1.0 / u_resolution) * float(u_amplitudes.length()))];
     float lowest = min(left, right);
     float dist = (amplitude - uv.y) * u_resolution.y;
 
     float a = 0.0;
     
-    if(abs(dist) <= u_line_thickness || (uv.y >= lowest && uv.y <= amplitude)) {
+    if(abs(dist) <= u_resolution.x * 0.005 * u_line_thickness || (uv.y >= lowest && uv.y <= amplitude)) {
       a = 1.0;
     } else if(dist > 0.0) {
       a = interpolate(1.0, u_fill_opacity, pow(1.0 - uv.y, 1.0 - u_opacity_falloff));
