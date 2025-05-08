fd731ed779fe680019a3245642ac1dfa06195626
Scanavium21 <Scanavium21@Protonmail.com>
2025-05-08 17:54:53 +0200

    Fix bar spectrum amplitude and spacing

--- src/renderer/shaders/components/SpectrumBarShader.ts
+++ src/renderer/shaders/components/SpectrumBarShader.ts
@@ -7,20 +7,21 @@ const shader = `
 
   void main(){
     vec2 uv = gl_FragCoord.xy / u_resolution;
-    // Twice the length to have space between bars and additional 2 on either end
-    float barWidth = 1.0 / float(2 * u_amplitudes.length() + 2);
+    // Twice the length to have space between bars and additional 1 on the end
+    float barWidth = 1.0 / float(2 * u_amplitudes.length() + 1);
 
     float bx = uv.x / barWidth;
     float amplitude = clamp(u_amplitudes[int(uv.x / (1.0 / float(u_amplitudes.length())))], -60.0, 0.0);
     // Map -60db -> 0db to 0 -> 1
-    float amp = (amplitude + 60.0) / 36.0 + 0.025;
+    float amp = (amplitude + 60.0) / 60.0 + 0.025;
+
+    // Distance from the center of the bar (0.0 for farthest and 1.0 for being at the center)
+    // float d = -abs(2.0 * fract(bx) - 1.0) + 1.0;
 
     float a = 0.0;
 
     // If every other bar (since each has one bar width around it)
     if(mod(floor(bx), 2.0) == 1.0 && abs(2.0 * uv.y - 1.0) <= amp) {
-      // Distance from the center of the bar (0.0 for farthest and 1.0 for being at the center)
-      // float d = - abs(2.0 * fract(bx) - 1.0) + 1.0;
       a = 1.0;
     }
     gl_FragColor = vec4(u_color * a, a);
