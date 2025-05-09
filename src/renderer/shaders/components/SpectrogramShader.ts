47e0f23c48a66fce3c3413d7aa667a22eaffd5f0
Scanavium21 <Scanavium21@Protonmail.com>
2025-05-09 17:59:01 +0200

    Fix spectrogram not showing at fractional sizes

--- src/renderer/shaders/components/SpectrogramShader.ts
+++ src/renderer/shaders/components/SpectrogramShader.ts
@@ -13,11 +13,11 @@ export const SpectrogramShader = `
     float amplitude = u_amplitudes[int(uv.y * float(u_amplitudes.length()))];
     amplitude = clamp(pow(amplitude, 2.0), 0.0, 1.0);
 
-    // If first column
+    // If last column
     if (gl_FragCoord.x >= u_resolution.x - 0.5) {
       gl_FragColor = vec4(u_color * amplitude, 1.0);
     } else {
-      // Get the color of the previous column (x-1)
+      // Get the color of the next column (x + 1)
       gl_FragColor = texture2D(u_backbuffer, vec2(uv.x + (1.0 / u_resolution.x), uv.y));
     }
   }
