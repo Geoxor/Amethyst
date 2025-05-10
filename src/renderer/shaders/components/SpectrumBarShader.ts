57ea49ef3d08127293c076419c4ec3dd1f3ab6f2
Scanavium21 <Scanavium21@Protonmail.com>
2025-05-10 09:30:22 +0200

    Added padding to bar spectrum visualizer

--- src/renderer/shaders/components/SpectrumBarShader.ts
+++ src/renderer/shaders/components/SpectrumBarShader.ts
@@ -4,16 +4,23 @@ const shader = `
   uniform vec2 u_resolution;
   uniform float[$BAR_COUNT] u_amplitudes;
   uniform vec3 u_color;
+  
+  float xPadding = 0.05;
 
-  void main(){
-    vec2 uv = gl_FragCoord.xy / u_resolution;
+  void main() {
+    float uvx = (gl_FragCoord.x - xPadding * u_resolution.x) / (u_resolution.x * (1.0 - 2.0 * xPadding));
+    if (uvx < 0.0 || uvx > 1.0) {
+      gl_FragColor = vec4(0.0);
+      return;
+    }
+    vec2 uv = vec2(uvx, gl_FragCoord.y / u_resolution.y);
     // Twice the length to have space between bars and additional 1 on the end
     float barWidth = 1.0 / float(2 * u_amplitudes.length() + 1);
 
     float bx = uv.x / barWidth;
     float amplitude = clamp(u_amplitudes[int(uv.x / (1.0 / float(u_amplitudes.length())))], -60.0, 0.0);
     // Map -60db -> 0db to 0 -> 1
-    float amp = (amplitude + 60.0) / 60.0 + 0.025;
+    float amp = (amplitude + 60.0) / 60.0 + 2.0 / u_resolution.y;
 
     // Distance from the center of the bar (0.0 for farthest and 1.0 for being at the center)
     // float d = -abs(2.0 * fract(bx) - 1.0) + 1.0;
