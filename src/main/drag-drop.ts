61285ac3929891d6862c28e105240c206ea4f6d6
Geoxor <34042825+Geoxor@users.noreply.github.com>
2025-05-14 03:38:12 +0300

    fix drag and drop

--- src/main/drag-drop.ts
+++ src/main/drag-drop.ts
@@ -4,9 +4,12 @@ import { ipcMain } from "electron/main";
 
 import { __dirname } from "./utility.js";
 
-const iconName = path.join(__dirname, "../../assets/images/audio64x64.png");
+ipcMain.on("ondragstart", async (event, filePath) => {
+  const { IS_DEV } = await import("./main.js");
+  const iconName = path.join(__dirname, IS_DEV ? "../../../assets/images/audio64x64.png" : "../../../../assets/images/audio64x64.png");
 
-ipcMain.on("ondragstart", (event, filePath) => {
+  if (!filePath) return;
+  console.log('started dragging', iconName)
   event.sender.startDrag({
     file: filePath,
     icon: iconName
