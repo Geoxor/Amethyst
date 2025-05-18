c3c7a786b6862144c3382c477453fd0cccb1a7d3
Geoxor <34042825+Geoxor@users.noreply.github.com>
2025-05-18 05:51:17 +0300

    show node count on output diagram

--- src/renderer/components/OutputDiagram/blobs/DspBlob.vue
+++ src/renderer/components/OutputDiagram/blobs/DspBlob.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useRouter } from "vue-router";
 
+import { amethyst } from "@/amethyst";
 import AmethystIcon from "@/icons/AmethystIcon.vue";
 
 import BlobLine from "./BlobLine.vue";
@@ -13,7 +14,7 @@ const router = useRouter();
   <generic-blob
     :title="$t('output_diagram.dsp.title')"
     clickable
-    subtitle="Amethyst DSP"
+    :subtitle="amethyst.player.nodeManager.nodes.value.length - 3 > 0 ? `Amethyst DSP\n${amethyst.player.nodeManager.nodes.value.length - 3} ${$t('output_diagram.dsp.node_count')}` : 'Amethyst DSP'"
     @click="router.push({ name: 'node-editor' })"
   >
     <amethyst-icon class="text-accent" />
