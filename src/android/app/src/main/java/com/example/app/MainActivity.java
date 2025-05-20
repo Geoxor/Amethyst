4f9d90fe7fd1308387b537470e905e25aa62941c
Nyabsi <nyabsi@sovellus.cc>
2025-05-20 22:15:33 +0300

    add required permission checks to Manifest and MainActivity

--- src/android/app/src/main/java/com/example/app/MainActivity.java
+++ src/android/app/src/main/java/com/example/app/MainActivity.java
@@ -1,5 +1,31 @@
 package com.example.app;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.core.app.ActivityCompat;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // check Android 13+ permissions for reading audio files.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_MEDIA_AUDIO
+            ) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(
+                        this,
+                        new String[]{Manifest.permission.READ_MEDIA_AUDIO},
+                        0
+                );
+            }
+        }
+    }
+}
