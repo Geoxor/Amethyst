de70479ebb12c400ceeeecc0dde4ad5a11fe4af0
Nyabsi <nyabsi@sovellus.cc>
2025-05-25 00:28:31 +0300

    make lastfm api handling easier to add new methods

--- src/renderer/logic/lastfm.ts
+++ src/renderer/logic/lastfm.ts
@@ -1,9 +1,27 @@
 import crypto from 'crypto';
+
 import {Amethyst} from "@/amethyst.js";
 
 const LAST_FM_API_KEY = "567fbeb21916c9bbbf33e57b4bec008f";
 const LAST_FM_SECRET_KEY = "91e6024ca99e46fa9232e4af110f446a";
 const LAST_FM_API_BASE = "https://ws.audioscrobbler.com/2.0";
+
+const LAST_FM_ERROR_CODE_MAP: Map<number, string> = new Map<number, string>([
+    [2, "Invalid service - This service does not exist"],
+    [3, "Invalid Method - No method with that name in this package"],
+    [4, "Authentication Failed - You do not have permissions to access the service"],
+    [5, "Invalid format - This service doesn't exist in that format"],
+    [6, "Invalid parameters - Your request is missing a required parameter"],
+    [7, "Invalid resource specified"],
+    [8, "Operation failed - Something else went wrong"],
+    [9, "Invalid session key - Please re-authenticate"],
+    [10, "Invalid API key - You must be granted a valid key by last.fm"],
+    [11, "Service Offline - This service is temporarily offline. Try again later."],
+    [13, "Invalid method signature supplied"],
+    [16, "There was a temporary error processing your request. Please try again"],
+    [26, "Suspended API key - Access for your account has been suspended, please contact Last.fm"],
+    [29, "Rate limit exceeded - Your IP has made too many requests in a short period"]
+]);
 
 const generateLastFmApiSignature = (params: Map<string, string>) => {
     const entries = Array.from(params.entries())
@@ -37,9 +55,33 @@ export class LastFm {
         return this.amethyst.state.settings.integrations.lastFm.enabled && this.amethyst.state.settings.integrations.lastFm.enableScrobbling;
     }
 
-    private handleServerErrorResponse(error: number) {
-        if (error == 9) {
-            this.needsReAuthentication = true;
+    private handleServerError(error: number, url: string) {
+        console.log(`%c[⚐ Last.fm]%c ${url} returned ${error}, ${LAST_FM_ERROR_CODE_MAP.get(error)}`, "background-color: #ff4800; color: black; font-weight: bold;", "color:rgb(255, 200, 0);");
+
+        switch (error) {
+            case 9:
+                this.needsReAuthentication = true;
+                break;
+            default:
+                break;
+        }
+    }
+
+    private async sendRequest(method: string, urlParams: string, signature: string): Promise<any> {
+
+        const methodUrl = `${LAST_FM_API_BASE}/?${urlParams}&api_sig=${signature}&format=json`;
+        const result = await fetch(
+            methodUrl,
+            { method: method, headers: { "User-Agent": `Amethyst/${this.amethyst.VERSION}` } },
+        );
+
+        const payload = await result.json();
+
+        if (payload["error"] == null) {
+            return payload;
+        } else {
+            this.handleServerError(payload["error"], methodUrl);
+            return null;
         }
     }
 
@@ -58,19 +100,12 @@ export class LastFm {
             const signature = generateLastFmApiSignature(params);
             const urlParams = encodeUrlParams(params);
 
-            const result = await fetch(
-                `${LAST_FM_API_BASE}/?${urlParams}&api_sig=${signature}&format=json`,
-                { method: "POST", headers: { "User-Agent": `Amethyst/${this.amethyst.VERSION}` } },
-            );
+            const result = await this.sendRequest("POST", urlParams, signature);
 
-            const payload = await result.json();
-
-            if (payload["error"] == null) {
-                console.log(`%c[⚐ Last.fm]%c Authenticated as ${payload["session"]["name"]}`, "background-color: #ff4800; color: black; font-weight: bold;", "color:rgb(255, 200, 0);", payload);
-                this.amethyst.state.settings.integrations.lastFm.sessionKey = payload["session"]["key"]
+            if (result !== null) {
+                console.log(`%c[⚐ Last.fm]%c Authenticated as ${result["session"]["name"]}`, "background-color: #ff4800; color: black; font-weight: bold;", "color:rgb(255, 200, 0);", result);
+                this.amethyst.state.settings.integrations.lastFm.sessionKey = result["session"]["key"]
                 return true;
-            } else {
-                console.log(`%c[⚐ Last.fm]%c Authenticated returned error`, "background-color: #ff4800; color: black; font-weight: bold;", "color:rgb(255, 200, 0);", payload);
             }
 
             return false;
@@ -94,19 +129,10 @@ export class LastFm {
                 const signature = generateLastFmApiSignature(params);
                 const urlParams = encodeUrlParams(params);
 
-                const result = await fetch(
-                    `${LAST_FM_API_BASE}/?${urlParams}&api_sig=${signature}&format=json`,
-                    { method: "POST", headers: { "User-Agent": `Amethyst/${this.amethyst.VERSION}` } },
-                );
+                const result = await this.sendRequest("POST", urlParams, signature);
 
-                const payload = await result.json();
-
-                if (payload["error"] == null) {
-                    console.log(`%c[⚐ Last.fm]%c Scrobble -> ${track} by ${artist} was ${payload["scrobbles"]["@attr"]["accepted"] > 0 ? "Accepted" : "Rejected"}`, "background-color: #ff4800; color: black; font-weight: bold;", "color:rgb(255, 200, 0);", payload);
-                } else {
-                    console.log(`%c[⚐ Last.fm]%c Scrobble returned error (${payload["error"]})`, "background-color: #ff4800; color: black; font-weight: bold;", "color:rgb(255, 200, 0);", payload);
-
-                    this.handleServerErrorResponse(payload["error"]);
+                if (result !== null) {
+                    console.log(`%c[⚐ Last.fm]%c Scrobble -> ${track} by ${artist} was ${result["scrobbles"]["@attr"]["accepted"] > 0 ? "Accepted" : "Rejected"}`, "background-color: #ff4800; color: black; font-weight: bold;", "color:rgb(255, 200, 0);", result);
                 }
             }
         });
